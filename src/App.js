27a9af0a80aaba86456289594cfe34ca1ae9f922
Tsix <haquoctoan021001@gmail.com>
2023-08-07 07:54:58 +0700

    Add new project food

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+
+
+
+  
+
+
+
+
+
 
 function App() {
+  const [color, setColor] = useState('yellow');
+  
+  function handleColor(e) {
+    setColor(
+      e.target.value 
+    )
+  }
+  function handleOnOff() {
+   color == 'yellow' || 'red '|| 'blue' ? setColor('#4b5c9c') : setColor('yellow')
+    
+    
+  }
+  
+  console.log(color)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className="rope-container">
     </div>
+    <div className="background" />
+    <div className="lamp" style={{backgroundColor: color}}>
+      <div className="lamp-reflection lamp-reflection-1" />
+      <div className="lamp-body" style={{backgroundColor: color}}>
+        <div className="lamp-body-bend-left" />
+        <div className="lamp-body-bend-right" />
+      </div>
+      <div className="lamb-nozzle" />
+      <div className="awesome">
+      <div className='btn-color'>
+        <label htmlFor="name">Chọn màu bóng đền: </label>
+        <input type="button" name="name" value="blue"  onClick={handleColor} />
+        <input type="button" name="name" value="red" onClick={handleColor} /> 
+        <input type="button" name="name" value="yellow" onClick={handleColor}/>   
+      </div>
+      <input type="button" name="button" value="On/Off" onClick={handleOnOff}/> 
+      
+    </div>
+    </div>
+    
+  </div>
+
+  
   );
 }
 
